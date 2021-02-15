f98c9c7b6e3171252f37604be1034046196e294a
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2021-02-15 00:52:37 -0300

    js editing

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//VARIABLES
+
+const slider = document.getElementById("slider")
+let sliderSection = document.querySelectorAll(".slider__section")
+let sliderSectionLast = sliderSection[sliderSection.length -1]
+const btnLeft = document.getElementById("btn-left")
+const btnRight = document.getElementById("btn-right")
+
+// INVESTIGAR INSERTADJACENTELEMENT
+
+// slider.insertAdjacentElement()
