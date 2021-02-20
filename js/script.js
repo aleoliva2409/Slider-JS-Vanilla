3e3af20648c1087f8dfd2a56d303a0e323ddea34
wozowski <alejandro.oliva2409@gmail.com>
2021-02-20 17:06:38 -0300

    last commit 20/2

--- js/script.js
+++ js/script.js
@@ -3,9 +3,25 @@
 const slider = document.getElementById("slider")
 let sliderSection = document.querySelectorAll(".slider__section")
 let sliderSectionLast = sliderSection[sliderSection.length -1]
-const btnLeft = document.getElementById("btn-left")
 const btnRight = document.getElementById("btn-right")
+const btnLeft = document.getElementById("btn-left")
 
-// INVESTIGAR INSERTADJACENTELEMENT
+slider.insertAdjacentElement("afterbegin",sliderSectionLast)
 
-// slider.insertAdjacentElement()
+const next = () => {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0]
+  slider.style.marginLeft = "-200%"
+  slider.style.transition = "all 0.5s"
+  setTimeout( () =>{
+    slider.style.transition = "none"
+    slider.insertAdjacentElement("beforeend" , sliderSectionFirst)
+    slider.style.marginLeft = "-100%"
+  }, 500)
+}
+
+
+
+btnRight.addEventListener("click" , () =>{
+  next()
+})
+
